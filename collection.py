ef3e7943b00436d3a0ad9a2814fd4e7cd9040902
yoezc <yoezcode@outlook.de>
2026-02-19 23:01:21 +0100

    working with lists, tuples and dictionaries

---
+++ collection.py
@@ -0,0 +1,34 @@
+myFruitList=["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+#change values in list
+myFruitList[2]="orange"
+print(myFruitList)
+
+myFinalAnswerTuple=("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+#access tuple by position
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+#access dictionary by name
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
