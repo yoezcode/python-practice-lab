9f84407216a21b4bfc59f207fbe711b3b2c27d21
yoezc <yoezcode@outlook.de>
2026-02-19 22:59:55 +0100

    string data types

---
+++ string-data-type.py
@@ -0,0 +1,15 @@
+myString= "This is a string"
+print(myString)
+print(type(myString))
+
+firstString="water"
+secondString="fall"
+thirdString= firstString+secondString
+print(thirdString)
+
+name=input("What is your name?")
+print("Hello " + name + "!")
+
+color=input("What's your favourit color? ")
+animal=input("What's your favourite animal? ")
+print("{}, you like a {} {}".format(name, color, animal))
